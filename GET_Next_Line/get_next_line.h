4d9196ec50085a28418a2d5f8fdb167995c8be33
Yoan Cucchi <102600896+YoanCucchi@users.noreply.github.com>
2022-03-29 18:46:50 +0300

    Add files via upload

---
+++ GET_Next_Line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycucchi <yoan066@yahoo.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 11:34:03 by ycucchi           #+#    #+#             */
+/*   Updated: 2022/01/25 10:08:58 by ycucchi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 42
+# define FD_SIZE 4096
+
+int	get_next_line(const int fd, char **line);
+
+#endif
